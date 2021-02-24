7d8bfdd8975032f53dcc074bd9f1f30b39135bac
Torear <Staryd797@yandex.ru>
2021-02-24 14:31:31 +0300

    add splash screen

---
+++ app/src/main/java/ru/torear/resume/App.kt
@@ -0,0 +1,13 @@
+package com.animo.ru
+
+import android.app.Application
+import com.orhanobut.hawk.Hawk
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Hawk.init(this).build()
+    }
+
+}
---
+++ app/src/main/java/ru/torear/resume/ui/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package ru.torear.resume.splash_screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+/*        startActivity(Intent(this, MainActivity::class.java))
+        finish()*/
+    }
+
+}
